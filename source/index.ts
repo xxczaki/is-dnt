2314558ee424eacdbff61e6ba0d5c12fbd74f004
Dario Vladovic <d.vladimyr@gmail.com>
2019-06-28 21:05:35 +0200

    Refactor code :wrench:

--- source/index.ts
+++ source/index.ts
@@ -1,21 +1,50 @@
 'use strict';
 
-/**
- * @returns {(boolean|undefined)} Whether the Do Not Track (DNT) policy is enabled in user's browser or not. `undefined` means the browser does not support DNT.
- */
-const isDnt = (): boolean | undefined => {
-	const nav: any = navigator;
-	const winext: any = window.external;
+const reTruthy = /^(?:1|yes|true)$/;
+const reFalsey = /^(?:0|no|false)$/;
 
-	if (window.doNotTrack || nav.doNotTrack || nav.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
-		if (window.doNotTrack === '1' || nav.doNotTrack === 'yes' || nav.doNotTrack === '1' || nav.msDoNotTrack === '1' || (winext.msTrackingProtectionEnabled && winext.msTrackingProtectionEnabled())) {
-			return true;
-		}
+const isFunction = <T>(arg: unknown): arg is T => typeof arg === 'function';
+const isUndefined = (arg: unknown): arg is undefined => arg === undefined;
 
+const yn = (arg: unknown): boolean | undefined => {
+	const str = String(arg).trim();
+
+	if (reTruthy.test(str)) {
+		return true;
+	}
+
+	if (reFalsey.test(str)) {
 		return false;
 	}
 
 	return undefined;
+};
+
+type FlagFunction = () => boolean;
+
+/**
+ * @returns {(boolean|undefined)} Whether the Do Not Track (DNT) policy is enabled in user's browser or not. `undefined` means the browser does not support DNT.
+ */
+const isDnt = (): boolean | undefined => {
+	const navigator = window.navigator as Navigator & { msDoNotTrack?: '0' | '1' };
+	const external = window.external as External & { msTrackingProtectionEnabled?: FlagFunction };
+
+	const results = [
+		window.doNotTrack,
+		navigator.doNotTrack,
+		navigator.msDoNotTrack,
+		isFunction<FlagFunction>(external.msTrackingProtectionEnabled) ?
+			external.msTrackingProtectionEnabled() :
+			undefined
+	]
+		.map(it => yn(it))
+		.filter(it => !isUndefined(it));
+
+	if (results.length <= 0) {
+		return;
+	}
+
+	return results.some(Boolean);
 };
 
 module.exports = isDnt;
